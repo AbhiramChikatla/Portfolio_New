dde25153bd410716e27f384a247284a54d057c9b
Chikatla Abhiram <abhiramchikatla03@gmail.com>
2025-01-10 00:36:03 +0530

    added client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client"; 
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
@@ -12,7 +11,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
-  return (
+  return ( 
     // <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-x-hidden mx-auto sm:px-10 px-5">
     <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
